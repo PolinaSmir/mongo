cd6b05f53506f5298e71bf6f839a2a505bcd81cc
Polina <smivasilisa@gmail.com>
2024-06-09 22:42:55 +0200

    Initialize server, add salad model

---
+++ controllers/salad.controller.js
@@ -0,0 +1,9 @@
+module.exports.createSalad = async (req, res, next) => {};
+
+module.exports.getAllSalads = async (req, res, next) => {};
+
+module.exports.getSalad = async (req, res, next) => {};
+
+module.exports.updateSalad = async (req, res, next) => {};
+
+module.exports.deleteSalad = async (req, res, next) => {};
